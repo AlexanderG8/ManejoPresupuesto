22c730bf3bf42cfd0d423e8992a795b6488fbc7c
Alexander Gomez <mpalexanderg@gmail.com>
2026-02-03 17:00:34 -0500

    Se agrego Clase y formulario para Tipo Cuenta

---
+++ Controllers/TiposCuentasController.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController: Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta model) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return View(model);
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/TipoCuenta.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ManejoPresupuesto.Models.Validations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta : IValidatableObject
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        //[PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Nombre != null && Nombre.Length > 0) 
+            {
+                var primeraLetra = Nombre[0].ToString();
+                if (primeraLetra != primeraLetra.ToUpper()) 
+                {
+                    /*Aquí estoy indicando que retorne un mensaje de error en el campo Nombre*/
+                    yield return new ValidationResult("La primera letra debe ser mayúscula", new[] { nameof(Nombre) });
+
+                    //yield return new ValidationResult("La primera letra debe ser mayúscula");
+                    /*Si en el caso no agrego el campo específico para que se muestre el error, esto lo toma como un error al nivel del modelo.
+                     Los errores a nivel del modelo son muy útiles cuando tienes un error que no tiene que ver exactamente 
+                    con un campo específico, sino que tiene que ver con algún tipo de acción general, como por ejemplo
+                    que quizás el usuario no tenga permiso para hacer una acción o algo por el estilo. */
+                }
+            }
+        }
+    }
+}
