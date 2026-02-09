acab7f974bc851f4a197cc177e3832af6388a889
Alexander Gomez <mpalexanderg@gmail.com>
2026-02-09 18:21:58 -0500

    Se culmino CRUD de Categorias

---
+++ Controllers/CategoriasController.cs
@@ -0,0 +1,99 @@
+﻿using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly IRepositorioCategorias repositorioCategorias;
+        private readonly IServicioUsuario servicioUsuario;
+        public CategoriasController(IRepositorioCategorias repositorioCategorias, IServicioUsuario servicioUsuario)
+        {
+            this.repositorioCategorias = repositorioCategorias;
+            this.servicioUsuario = servicioUsuario;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var usuarioId = servicioUsuario.ObtenerUsuarioId();
+            var categorias = await repositorioCategorias.Obtener(usuarioId);
+            return View(categorias);
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(Categoria categoria)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(categoria);
+            }
+
+            var usuarioId = servicioUsuario.ObtenerUsuarioId();
+            categoria.UsuarioId = usuarioId;
+            await repositorioCategorias.Crear(categoria);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Editar(int id)
+        {
+            var usuarioId = servicioUsuario.ObtenerUsuarioId();
+            var categoria = await repositorioCategorias.ObtenerPorId(id, usuarioId);
+            if (categoria is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            return View(categoria);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(Categoria categoriaEditar)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(categoriaEditar);
+            }
+            var usuarioId = servicioUsuario.ObtenerUsuarioId();
+            var categoria = await repositorioCategorias.ObtenerPorId(categoriaEditar.Id, usuarioId);
+            if (categoria is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            categoriaEditar.UsuarioId = usuarioId;
+            await repositorioCategorias.Actualizar(categoriaEditar);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Borrar(int id)
+        {
+            var usuarioId = servicioUsuario.ObtenerUsuarioId();
+            var categoria = await repositorioCategorias.ObtenerPorId(id, usuarioId);
+            if (categoria is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            return View(categoria);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BorrarCategoria(int id)
+        {
+            var usuarioId = servicioUsuario.ObtenerUsuarioId();
+            var categoria = await repositorioCategorias.ObtenerPorId(id, usuarioId);
+            if (categoria is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            await repositorioCategorias.Borrar(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
